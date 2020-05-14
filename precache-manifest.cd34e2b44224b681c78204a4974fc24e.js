99176fe5f12206ce71ba82f70465ac8222b0f5ea
Vitalii Korol <vitaliikorol@192-168-0-100.rdsnet.ro>
2020-05-14 15:16:03 +0300

    Updates

---
+++ precache-manifest.cd34e2b44224b681c78204a4974fc24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36c71c99c274f2612df494c0c7316df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "52b20815151e87368e94",
+    "url": "./static/css/main.0dc19ffa.chunk.css"
+  },
+  {
+    "revision": "0f04c05271367144c83f",
+    "url": "./static/js/2.ce344a51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ce344a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b20815151e87368e94",
+    "url": "./static/js/main.859db2ac.chunk.js"
+  },
+  {
+    "revision": "319a3c468d8ecd2a5698",
+    "url": "./static/js/runtime-main.e5e6300a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.cd34e2b44224b681c78204a4974fc24e.js"
 );
 
 self.addEventListener('message', (event) => {
